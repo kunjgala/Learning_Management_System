40ba3e15fd3450f376dc3d02ffffa4531810b91b
neelayj99 <60360719+neelayj99@users.noreply.github.com>
2020-10-13 14:45:18 +0530

    Add files via upload

---
+++ Profile/Edit Profile.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Employee Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="profile.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<nav class="navbar-expand-lg navbar navbar-dark bg-primary">
+	  <a class="navbar-brand" href="#">TRAICORD</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item">
+	        <a class="nav-link" href="#" id="backwhite">Home <span class="sr-only">(current)</span></a>
+	      </li>
+	    </ul>
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item">
+	        <a class="nav-link" id="backwhite" href="#">Application Form<span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	      	<a class="nav-link" id="logout" href="#">LOGOUT</a>
+	      </li>
+	    </ul>
+	   
+	  </div>
+    </nav>
+<center><a href="DOg.jpg"><img src="DOg.jpg"></a></center>
+<center>Click to view your Profile Photo/Edit Profile Photo</center>
+<br><br>
+<form><center>
+    Name:<br> <input type="text" name="name" placeholder="Peter Sullivan"><br><br>
+    Email:<br> <input type="text" name="email" placeholder="petersullivan@outlook.com"><br><br>
+    Employee Type:<br> <input type="text" name="type" placeholder="HOD"><br><br>
+    Phone Number:<br> <input type="text" name="phone" placeholder="9982434342"><br><br>
+    New Password:<br> <input type="password" name="pass" placeholder="*******"><br><br>
+    Confirm Password:<br> <input type="password" name="pass" placeholder="*******"><br><br>
+    <input type="submit" value="Save and Continue">
+</form></center>
+
+
+
+
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+  
+
+
+
+
+
+</body>
+</html>
